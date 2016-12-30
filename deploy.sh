5e8d067f25e602b169c219cea64280ad7bb96bdc
artikow <artikow@gmail.com>
2016-12-30 09:19:33 +0700

    modify unix deploy script -> user deploy.sh build & deploy.sh jmeter_folder

--- deploy.sh
+++ deploy.sh
@@ -1,14 +1,21 @@
 #!/bin/sh
-
-# mvn dependency:copy-dependencies install -DexcludeGroupIds=org.apache.jmeter
-
 JAR_LIB=jmeter-jpos-components-1.0.0-SNAPSHOT.jar
 
 if [ $# -eq 0 ]; then
-	echo "please input jmeter root folder"
+	echo "please input build to compile or jmeter root folder to deploy"
+	echo "example: sh deploy.sh build"
+	echo "example: sh deploy.sh /var/jmeter"
 	exit;
 fi
 
-cp -Rf $1/lib $1/lib-ori
+if [ $1 == "build" ]; then
+	mvn dependency:copy-dependencies install -DexcludeGroupIds=org.apache.jmeter
+	exit;
+fi
+
+if [ ! -d $1/lib-ori ]; then
+	cp -Rf $1/lib $1/lib-ori
+fi
+
 cp -Rf target/${JAR_LIB} $1/lib/ext/${JAR_LIB}
 cp -Rf target/dependency/* $1/lib
